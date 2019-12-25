c2f600fafa988f76021dc6beec6c7cb769902c7c
asilnikov <a.silnikov@inbox.ru>
2019-12-25 15:20:47 +0000

    v.1.2

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ class Matrix():
     def __init__(self, M_сol, N_str):
         self.M_сol = M_сol
         self.N_str = N_str
-        self.__initialization_matrix1 = None
-        self.__initialization_matrix2 = None
+        self.__initialization_matrix = None
         self.__operations = None
         self.__transpose = None
         self.__is_square = None
@@ -16,22 +15,17 @@ class Matrix():
         self.__is_zero = None
         self.__is_diagonal = None
         self.__output = None
-        self.__download = 'test'
 
-    @property
-    def initialization_matrix1(self):
-        return self.__initialization_matrix1
-
-    @initialization_matrix1.setter
-    def initialization_matrix1(self, matrix):
+   
+    def initialization_matrix(self, matrix):
         self.matrix = matrix
         count = 0
         list = self.matrix
         for item in list:
             count += len(item)
-        sum_args = self.M_сol * self.N_str
-        if count == sum_args:
-            self.__initialization_matrix1 = np.array(self.matrix).reshape(
+        add_args = self.M_сol * self.N_str
+        if count == add_args:
+             self.__initialization_matrix = np.array(self.matrix).reshape(
                 self.M_сol, self.N_str)
         else:
             print(
@@ -39,34 +33,11 @@ class Matrix():
                         'red'), colored(int(count), 'red'),
                 colored("to form:", 'red'), colored(int(self.M_сol), 'red'),
                 colored('x', 'red'), colored(int(self.N_str), 'red'))
-        #print(self.__initialization_matrix1)
-
-    @property
-    def initialization_matrix2(self):
-        return self.__initialization_matrix2
-
-    @initialization_matrix2.setter
-    def initialization_matrix2(self, matrix):
-        self.matrix = matrix
-        count = 0
-        list = self.matrix
-        for item in list:
-            count += len(item)
-        sum_args = self.M_сol * self.N_str
-        if count == sum_args:
-            self.__initialization_matrix2 = np.array(self.matrix).reshape(
-                self.M_сol, self.N_str)
-        else:
-            print(
-                colored("Input error! Cannot convert an array of size:",
-                        'red'), colored(int(count), 'red'),
-                colored("to form:", 'red'), colored(int(self.M_сol), 'red'),
-                colored('x', 'red'), colored(int(self.N_str), 'red'))
-        #print(self.__initialization_matrix2)
+        return  self.__initialization_matrix
 
     def is_identity(self):
         A = np.identity(self.N_str)
-        B = self.__initialization_matrix1
+        B =  self.__initialization_matrix
         equal = np.allclose(A, B)
         print(equal)
         return self.__is_identity
@@ -80,15 +51,15 @@ class Matrix():
 
     def is_zero(self):
         A = np.zeros((self.M_сol, self.N_str))
-        B = self.__initialization_matrix1
+        B =  self.__initialization_matrix
         equal = np.allclose(A, B)
         print(equal)
         return self.__is_zero
 
     def is_diagonal(self):
-        A = np.diagflat(self.__initialization_matrix1)
+        A = np.diagflat( self.__initialization_matrix)
         ta = np.trace(A)
-        B = self.__initialization_matrix1
+        B =  self.__initialization_matrix
         tb = np.trace(B)
         if fabs(ta) == fabs(tb):
             print('True')
@@ -97,26 +68,16 @@ class Matrix():
             print('False')
 
     def transpose(self, matrix):
-        self.M1 = self.__initialization_matrix1
-        self.M2 = self.__initialization_matrix2
+        self.matrix = matrix
         if matrix == 'M1':
             M1 = np.transpose(self.M1)
             print(M1)
             return M1
-        if matrix == 'M2':
-            M2 = np.transpose(self.M2)
-            print(M2)
-            return M2
 
-    @property
-    def operations(self):
-        return self.__operations
-
-    @operations.setter
-    def operations(self, operator):
+    def operations(self, operator, M2):
         self.operator = operator
-        M1 = self.__initialization_matrix1
-        M2 = self.__initialization_matrix2
+        M1 =  self.__initialization_matrix
+        self.M2 = M2
         if self.operator == '+':
             M = M1 + M2
         if self.operator == '-':
@@ -124,57 +85,72 @@ class Matrix():
         if self.operator == '**':
             M = M1**M2
         if self.operator == '*':
-            M = M1 * M2
+            M = M1.dot(M2)
         if self.operator == '/':
             M = M1 / M2
         print(M)
+        return self.__operations
 
-    
-  
-    
     def output(self):
-        M = self.__initialization_matrix1
-        a = []
-        for a in M:
-            for matrix in a:
+        M = self.__initialization_matrix
+        for row in M:
+            for matrix in row:
                 print(matrix, end=' ')
             print()
-          
-         
 
-   
+
+class HorizontalVector(Matrix):
+    def __init__(self, N_str, M_col = None):
+      super().__init__(N_str, M_col)   
+
+    
+    def initialization_matrix(self, matrix):
+      self.matrix = matrix
+      count = 0
+      list = self.matrix
+      for item in list:
+          count += len(str(item))
+      column = self.M_сol    
+      if count == column:
+        self.__initialization_matrix = np.array(self.matrix).reshape( 1 , self.M_сol)
+        print(self.__initialization_matrix)
+        return self.__initialization_matrix
+      else:
+        print(colored("Vector dimension =", "red"), colored(int(count), "red"), colored(". Error! enter the correct vector dimension.", 'red'))
+  
+class VerticalVector(Matrix):
+    def __init__(self, M_col, N_str = None):
+      super().__init__(N_str, M_col)   
+
+    
+    def initialization_matrix(self, matrix):
+      self.matrix = matrix
+      count = 0
+      list = self.matrix
+      for item in list:
+          count += len(str(item))
+      string = self.N_str    
+      if count == string:
+        self.__initialization_matrix = np.array(self.matrix).reshape( self.N_str, 1 )
+        print(self.__initialization_matrix)
+        return self.__initialization_matrix
+      else:
+        print(colored("Vector dimension =", "red"), colored(int(count), "red"), colored(". Error! enter the correct vector dimension.", 'red')) 
 
 
 test = Matrix(3, 3)
-test.initialization_matrix1 = [[2, 0, 0], [0, -158, 0], [0, 0, -38]]
-test.initialization_matrix2 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+test.initialization_matrix([[16, 2, 2], [2, 2, 2], [2, 2, 2]])
 
-#test.operations = '-'
-#test.transpose('M1')
-test.is_square()
+test.operations('*', [[2, 2, 2], [2, 2, 2], [2, 2, 2]])
+test.transpose([[2, 0, 0], [0, -158, 0], [0, 0, -38]])
+#test.is_square()
 #test.is_identity()
 #test.is_zero()
 #test.is_diagonal()
 #test.output()
 
+test3 = VerticalVector(4)
+test3.initialization_matrix([2, 1, 1, 5])
 
-class HorizontalVector(Matrix):
-    def __init__(self, M_сol, N_str):
-        super().__init__(M_сol, N_str)
-
-
-test2 = HorizontalVector(4, 3)
-test2.initialization_matrix1 = [[2, 1, 1], [3, 3, 4], [5, 3, 8], [6, 9, 7]]
-
-#test2.output()
-
-
-class VerticalVector(Matrix):
-    def __init__(self, M_сol, N_str):
-        super().__init__(M_сol, N_str)
-
-
-test3 = VerticalVector(4, 4)
-test3.initialization_matrix1 = [[2, 1, 1, 768], [3, 3, 4, 123], [5, 3, 8, 135],
-                                [6, 9, 7, 987]]
-#test3.output
+test2 = HorizontalVector(4)
+test2.initialization_matrix([2, 1, 1, 5])
